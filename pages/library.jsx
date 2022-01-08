ade47573101e89b61dbc9be64f5fe70d2a29f09e
Muhlis Iqbal Utomo <iqbalutomo@Muhliss-MacBook-Pro.local>
2022-01-08 14:13:35 +0700

    add axios & library page

--- pages/library.jsx
+++ pages/library.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from '../components/Modal';
 import Nav from '../components/Nav';
 import useStore from '../lib/store';
 
 export default function library() {
   const state = useStore();
+  const [bookFilter, setBookFilter] = useState('all');
 
   return (
     <div className="h-screen">
       <Nav />
       <div className="flex justify-between items-center">
         <h1 className="text-2xl">Library Page</h1>
-        <select className="py-1 px-2 rounded-md bg-pink-400 text-white">
+        <select
+          value={bookFilter}
+          onChange={(e) => setBookFilter(e.target.value)}
+          className="py-1 px-2 rounded-md bg-pink-400 text-white"
+        >
           <option defaultValue value="all">
             All
           </option>
@@ -21,66 +26,55 @@ export default function library() {
         </select>
       </div>
       <div className="grid grid-cols-2 gap-8 items-center mt-5 pb-10">
-        <img
-          onClick={() => state.setModal(true)}
-          className="rounded-md"
-          src="/cover.jpeg"
-          width={250}
-        />
-        <img
-          onClick={() => state.setModal(true)}
-          className="rounded-md"
-          src="/cover.jpeg"
-          width={250}
-        />
-        <img
-          onClick={() => state.setModal(true)}
-          className="rounded-md"
-          src="/cover.jpeg"
-          width={250}
-        />
-        <img
-          onClick={() => state.setModal(true)}
-          className="rounded-md"
-          src="/cover.jpeg"
-          width={250}
-        />
-        <img
-          onClick={() => state.setModal(true)}
-          className="rounded-md"
-          src="/cover.jpeg"
-          width={250}
-        />
-        <img
-          onClick={() => state.setModal(true)}
-          className="rounded-md"
-          src="/cover.jpeg"
-          width={250}
-        />
-        <img
-          onClick={() => state.setModal(true)}
-          className="rounded-md"
-          src="/cover.jpeg"
-          width={250}
-        />
-        <img
-          onClick={() => state.setModal(true)}
-          className="rounded-md"
-          src="/cover.jpeg"
-          width={250}
-        />
-        <img
-          onClick={() => state.setModal(true)}
-          className="rounded-md"
-          src="/cover.jpeg"
-          width={250}
-        />
-        <img
-          onClick={() => state.setModal(true)}
-          className="rounded-md"
-          src="/cover.jpeg"
-          width={250}
-        />
+        {bookFilter === 'all' && (
+          <>
+            <img
+              onClick={() => state.setModal(true)}
+              className="rounded-md"
+              src="/cover.jpeg"
+              width={250}
+            />
+            <img
+              onClick={() => state.setModal(true)}
+              className="rounded-md"
+              src="/cover.jpeg"
+              width={250}
+            />
+            <img
+              onClick={() => state.setModal(true)}
+              className="rounded-md"
+              src="/cover.jpeg"
+              width={250}
+            />
+          </>
+        )}
+        {bookFilter === 'readingList' && (
+          <>
+            <img
+              onClick={() => state.setModal(true)}
+              className="rounded-md"
+              src="/cover.jpeg"
+              width={250}
+            />
+          </>
+        )}
+        {bookFilter === 'readingNow' && <></>}
+        {bookFilter === 'finished' && (
+          <>
+            <img
+              onClick={() => state.setModal(true)}
+              className="rounded-md"
+              src="/cover.jpeg"
+              width={250}
+            />
+            <img
+              onClick={() => state.setModal(true)}
+              className="rounded-md"
+              src="/cover.jpeg"
+              width={250}
+            />
+          </>
+        )}
       </div>
       {state.modal && <Modal />}
     </div>
