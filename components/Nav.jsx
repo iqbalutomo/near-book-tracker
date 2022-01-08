4cb18f65f820ef6e263b3a2cfc2329f238ec9f8f
Muhlis Iqbal Utomo <iqbalutomo@Muhliss-MacBook-Pro.local>
2022-01-08 16:17:00 +0700

    add edit modal profile

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,10 @@ export default function Nav() {
   const [isLogin, setIsLogin] = useState(true);
   const [modalProfile, setModalProfile] = useState(false);
   const [modalEditProfile, setModalEditProfile] = useState(false);
+  const [userName, setUserName] = useState('john doe');
+  const [bioProfile, setBioProfile] = useState(
+    'Book my life. instagram: @iqbalutomo'
+  );
 
   return (
     <div>
@@ -41,7 +45,7 @@ export default function Nav() {
                 onClick={() => setModalProfile(true)}
                 className="flex justify-between gap-4 items-center font-bold py-2 px-2 bg-[#16ADAD] text-white rounded-md"
               >
-                <div className="flex">
+                <div className="flex gap-1 items-center">
                   <img src="/assets/icons/Nomads Avatar.png" width={30} />
                   <p className="font-thin">John Doe</p>
                 </div>
@@ -67,7 +71,15 @@ export default function Nav() {
                     className="absolute inset-0"
                   ></div>
                   <div className="absolute z-10 mt-2 py-2 px-3 pr-14 font-thin text-white bg-[#16ADAD] rounded-md">
-                    <button className="my-2">Edit Profile</button>
+                    <button
+                      onClick={() => {
+                        setModalEditProfile(true);
+                        setModalProfile(false);
+                      }}
+                      className="my-2"
+                    >
+                      Edit Profile
+                    </button>
                     <br />
                     <button className="mb-2">Logout</button>
                   </div>
@@ -109,6 +121,58 @@ export default function Nav() {
                 <Link href="/profile">Profile</Link>
               </li>
             </ul>
+          </div>
+        </div>
+      )}
+      {modalEditProfile && (
+        <div className="absolute top-0 left-0">
+          <div className="fixed z-20 w-full h-full bg-black bg-opacity-70">
+            <div className="absolute top-[10%] right-0 left-0 bg-white m-4 rounded-md">
+              <div className="text-center">
+                <h1 className="text-2xl mt-8 mb-5">Edit Profile </h1>
+                <div className="shadow-lg p-2 rounded-full absolute top-4 right-4 ">
+                  <svg
+                    onClick={() => setModalEditProfile(false)}
+                    class="w-5 h-5"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    ></path>
+                  </svg>
+                </div>
+                <img
+                  src="/assets/icons/Nomads Avatar.png"
+                  width={150}
+                  className="mx-auto mb-4"
+                />
+                <form action="" className="px-4">
+                  <input
+                    type="text"
+                    onChange={(e) => setUserName(e.target.value)}
+                    value={userName}
+                    className="text-[#16ADAD] font-bold text-sm bg-pink-200 w-full py-2 px-2 rounded-md mb-4"
+                  />
+                  <textarea
+                    type="text"
+                    onChange={(e) => setBioProfile(e.target.value)}
+                    value={bioProfile}
+                    className="text-[#16ADAD] font-bold text-sm bg-pink-200 w-full py-2 px-2 rounded-md mb-8 resize-none h-28"
+                  />
+                  <input
+                    type="submit"
+                    value="Edit Profile"
+                    className="text-white text-xl font-bold bg-pink-400 w-full py-4 px-2 rounded-md mb-4"
+                  />
+                </form>
+              </div>
+            </div>
           </div>
         </div>
       )}
